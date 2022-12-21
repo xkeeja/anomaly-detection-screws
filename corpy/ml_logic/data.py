b5ab67d81b587556f0386c5bc717a064a49991e9
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2022-12-21 22:13:15 +0900

    data preprocessing & loading

--- corpy/ml_logic/data.py
+++ corpy/ml_logic/data.py
@@ -1,37 +1,53 @@
-import os
-import cv2
-import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
-def load_data(path):
-    g_files = []
-    ng_files = []
-    X, y = [], []
+def convert_to_float(image):
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    return image
 
-    # get file paths for all good images
-    g_dir = os.path.join(path,'train','good')
-    for filename in os.listdir(g_dir):
-        f = os.path.join(g_dir, filename)
-        g_files.append(f)
 
-    # load good images
-    for file in g_files:
-        X.append(cv2.imread(file))
-        y.append(0)
+def trans1(img):
+    return tfa.image.rotate(tf.image.flip_left_right(tf.image.flip_up_down(img)),-.2,fill_mode="reflect",interpolation="bilinear")
 
-    # get file paths for all not-good images
-    ng_dir = os.path.join(path,'train','not-good')
-    for filename in os.listdir(ng_dir):
-        f = os.path.join(ng_dir, filename)
-        ng_files.append(f)
 
-    # load not-good images
-    for file in ng_files:
-        X.append(cv2.imread(file))
-        y.append(1)
+def trans2(img):
+    return tfa.image.rotate(img,-.2,fill_mode="reflect",interpolation="bilinear")
 
-    c = list(zip(X, y))
-    np.random.shuffle(c)
-    X, y = zip(*c)
 
-    return np.array(X), np.array(y)
+def trans3(img):
+    return tfa.image.rotate(img,.2,fill_mode="reflect",interpolation="bilinear")
+
+
+def load_data(batch_size, image_size, data_type):
+    types = {
+        'train': 'train/good',
+        'test': 'test',
+        'anomaly_only': 'train/not-good'
+    }
+
+    dim = (image_size, image_size)
+
+    ds = image_dataset_from_directory(
+        f'data/archive/{types[data_type]}',
+        labels=None,
+        image_size=dim,
+        interpolation='nearest',
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    if data_type == 'train':
+        ds1, dst1, dst2, dst3 = ds, ds.map(trans1), ds.map(trans2), ds.map(trans3)
+
+        ds = ds1.concatenate(dst1).concatenate(dst2).concatenate(dst3)
+
+    ds = (
+        ds
+        .map(convert_to_float)
+        .cache()
+        .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    )
+
+    return ds
